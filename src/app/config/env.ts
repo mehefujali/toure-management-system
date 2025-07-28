1c99fb351122ec5f5a5fdd72a9fb1d0f822e9549
mehefuj ali <mehefujalim@gmail.com>
2025-07-29 02:53:48 +0530

    setup complited

---
+++ src/app/config/env.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface EnvConfig {
+  PORT: string;
+  MONGO_URI: string;
+  NODE_ENV: "development" | "production";
+}
+
+const loadEnvVariables = (): EnvConfig => {
+  const requiredEnv = ["PORT", "MONGO_URI", "NODE_ENV"];
+  requiredEnv.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`MIssing require env ${key}`);
+    }
+  });
+  return {
+    PORT: process.env.PORT as string,
+    MONGO_URI: process.env.MONGO_URI as string,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
+  };
+};
+
+export const envVars = loadEnvVariables();
